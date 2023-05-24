8143e358ccdbeda14af9234338e34b91ccca1f4a
Jhon-Giraldo <jhon.giraldo@pragma.com.co>
2023-05-24 16:37:25 -0500

    Creating Animated Ball Animation

--- lib/ui/widgets/ball/animated_ball_widget.dart
+++ lib/ui/widgets/ball/animated_ball_widget.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
 
-class AnimatedBall extends StatelessWidget {
-  const AnimatedBall({
-    Key? key,
-  }) : super(key: key);
+class AnimatedBall extends StatefulWidget {
+  const AnimatedBall({super.key});
+
+  @override
+  State<AnimatedBall> createState() => _AnimatedBallState();
+}
+
+class _AnimatedBallState extends State<AnimatedBall>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animatedController;
+  late Animation<Offset> animatedBall;
+
+  @override
+  void initState() {
+    super.initState();
+    animatedController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    //Animated Ball KeyFrame 1
+    animatedBall = Tween<Offset>(
+      begin: const Offset(0, 0),
+      end: const Offset(2, 1),
+    ).animate(animatedController)
+      ..addListener(() {
+        setState(() {});
+      });
+    animatedController.repeat(reverse: false);
+    animatedController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    animatedController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +54,11 @@ class AnimatedBall extends StatelessWidget {
           padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
           width: 400,
           height: 280,
-          child: CustomPaint(
-            painter: _AnimatedBallPainter(),
+          child: SlideTransition(
+            position: animatedBall,
+            child: CustomPaint(
+              painter: _AnimatedBallPainter(),
+            ),
           ),
         ),
       ),
@@ -38,7 +75,33 @@ class _AnimatedBallPainter extends CustomPainter {
       ..style = PaintingStyle.stroke;
 
     canvas.drawCircle(
-        Offset(size.width / 9, size.height / 9), size.width / 14, paint);
+        Offset(size.width / 9, size.height / 9), size.width / 18, paint);
+
+    final paintKeyFrames = Paint()
+      ..color = Colors.grey.shade400
+      ..strokeWidth = 2
+      ..style = PaintingStyle.stroke;
+
+    canvas.drawCircle(Offset(size.width / 4.8, size.height / 2.8),
+        size.width / 18, paintKeyFrames);
+
+    canvas.drawCircle(Offset(size.width / 3.3, size.height / 1.7),
+        size.width / 18, paintKeyFrames);
+
+    canvas.drawCircle(Offset(size.width / 2.5, size.height / 1.2),
+        size.width / 18, paintKeyFrames);
+
+    canvas.drawCircle(Offset(size.width / 1.85, size.height / 1.57),
+        size.width / 18, paintKeyFrames);
+
+    canvas.drawCircle(Offset(size.width / 1.5, size.height / 2.3),
+        size.width / 18, paintKeyFrames);
+
+    canvas.drawCircle(Offset(size.width / 1.27, size.height / 1.64),
+        size.width / 18, paintKeyFrames);
+
+    canvas.drawCircle(Offset(size.width / 1.12, size.height / 1.25),
+        size.width / 18, paintKeyFrames);
   }
 
   @override
