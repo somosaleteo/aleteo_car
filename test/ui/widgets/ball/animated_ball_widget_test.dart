1103912f53f20d2e5593a9c05e5fde3918e5056e
JimenaLeyva07 <jimena.leyva@pragma.com.co>
2023-06-07 15:25:08 -0500

    ball test fixed

--- test/ui/widgets/ball/animated_ball_widget_test.dart
+++ test/ui/widgets/ball/animated_ball_widget_test.dart
@@ -1,62 +1,59 @@
-import 'package:aleteo_car/ui/widgets/buttons/animated_icon_button_widget.dart';
+import 'package:aleteo_car/ui/widgets/ball/animated_ball_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  // Inicializando el entorno de prueba para el widget
-  TestWidgetsFlutterBinding.ensureInitialized();
+  // Prueba de widget para verificar si AnimatedBall se crea correctamente.
+  testWidgets('AnimatedBall should create its State',
+      (WidgetTester tester) async {
+    const animatedBall = AnimatedBall();
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: animatedBall,
+      ),
+    );
+    expect(find.byType(SlideTransition), findsOneWidget);
+  });
+  // Prueba para verificar si AnimatedBallPainter dibuja correctamente
+  testWidgets('AnimatedBallPainter should paint', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+          home: CustomPaint(
+        painter: AnimatedBallPainter(1.0),
+      )),
+    );
+    expect(
+        find.byWidgetPredicate((widget) =>
+            widget is CustomPaint && widget.painter is AnimatedBallPainter),
+        findsOneWidget);
+  });
 
-  group('AnimatedIconButtonWidget', () {
-    testWidgets('debería ser creado', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        const MaterialApp(
-          home: AnimatedIconButtonWidget(),
-        ),
-      );
-      expect(find.byType(AnimatedIconButtonWidget), findsOneWidget);
-    });
-
-    testWidgets('debería contener un MaterialButton',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(
-        const MaterialApp(
-          home: AnimatedIconButtonWidget(),
-        ),
-      );
-      expect(find.byType(MaterialButton), findsOneWidget);
-    });
-
-    testWidgets('debería contener un AnimatedContainer',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(
-        const MaterialApp(
-          home: AnimatedIconButtonWidget(),
-        ),
-      );
-      expect(find.byType(AnimatedContainer), findsOneWidget);
-    });
-
-    testWidgets('debería contener un AnimatedIcon',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(
-        const MaterialApp(
-          home: AnimatedIconButtonWidget(),
-        ),
-      );
-      expect(find.byType(AnimatedIcon), findsOneWidget);
-    });
-
-    testWidgets('debería ejecutar la función onPressed cuando se presiona',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(
-        const MaterialApp(
-          home: AnimatedIconButtonWidget(),
-        ),
-      );
-      await tester.tap(find.byType(MaterialButton));
-      await tester.pump();
-      // Esta prueba verifica si la función onPressed se llama cuando se presiona MaterialButton.
-      // Se puede ampliar en función del comportamiento real de la función onPressed.
-    });
+  // Prueba para verificar si el AnimatedBall se deshace correctamente
+  testWidgets('AnimatedBall should dispose AnimationController when disposed',
+      (WidgetTester tester) async {
+    const animatedBall = AnimatedBall();
+    await tester.pumpWidget(const MaterialApp(
+      home: animatedBall,
+    ));
+    final state = tester.state<State<AnimatedBall>>(find.byType(AnimatedBall));
+    expect(state, isNotNull);
+    await tester.pumpWidget(Container());
+    expect(state.mounted, isFalse);
+  });
+  testWidgets('AnimatedBall should dispose AnimationController when disposed',
+      (WidgetTester tester) async {
+    const animatedBall = AnimatedBall();
+    await tester.pumpWidget(const MaterialApp(
+      home: animatedBall,
+    ));
+    final state = tester.state<State<AnimatedBall>>(find.byType(AnimatedBall));
+    expect(state, isNotNull);
+    await tester.pumpWidget(Container());
+    expect(state.mounted, isFalse);
+  });
+  testWidgets('shouldRepaint returns false', (WidgetTester tester) async {
+    final myPainter = AnimatedBallPainter(1.0);
+    final oldMyPainter = AnimatedBallPainter(2.0);
+    expect(myPainter.shouldRepaint(oldMyPainter), false);
   });
 }
